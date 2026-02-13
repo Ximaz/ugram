922d54d86fd02103a114e1d544e64307aaa654d3
Clement-Z4RM <clement.lienard@epitech.eu>
2026-02-13 18:52:27 -0500

    style: change Prettier configuration and ensure LF on all OSs

--- packages/frontend/src/routes/(auth)/signup/components/Phone.svelte
+++ packages/frontend/src/routes/(auth)/signup/components/Phone.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
-	import * as Field from "$lib/components/ui/field/index.js";
-	import { Input } from "$lib/components/ui/input/index.js";
-	import { signUp } from "$lib/remotes/auth.remote";
+  import * as Field from "$lib/components/ui/field/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+  import { signUp } from "$lib/remotes/auth.remote";
 </script>
 
 <Field.Field>
-	<Field.Label for="phone">Phone</Field.Label>
-	<Input {...signUp.fields.phoneNumber.as("tel")} id="phone" placeholder="(123) 456-7890" required />
+  <Field.Label for="phone">Phone</Field.Label>
+  <Input
+    {...signUp.fields.phoneNumber.as("tel")}
+    id="phone"
+    placeholder="(123) 456-7890"
+    required
+  />
 </Field.Field>
