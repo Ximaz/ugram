9375391ab182b87ea48c05dce627311e825dfccf
Clement-Z4RM <clement.lienard@epitech.eu>
2026-02-15 15:14:36 -0500

    feat(frontend): add user mention functionality in post creation

--- packages/frontend/src/lib/remotes/post.remote.ts
+++ packages/frontend/src/lib/remotes/post.remote.ts
@@ -5,19 +5,31 @@ import { query, form, getRequestEvent } from "$app/server";
 import { API_URL } from "$env/static/private";
 import { postCreateSchema, postImageUploadSchema } from "backend/schemas";
 import { error, invalid, redirect } from "@sveltejs/kit";
+import { getUsers } from "$lib/remotes/user.remote";
 
 function getRandomArbitrary(min = 100, max = 1000) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+async function getMentionId(mention: string) {
+  const { users } = await getUsers({ search: mention, limit: 1 });
+
+  if (!users.length || users[0].username !== mention) return null;
+
+  return users[0].id;
+}
+
 const createPostSchema = postCreateSchema
   .extend(postImageUploadSchema.shape)
-  .extend({ keywords: z.string().optional(), mention: z.string().optional() }) // TODO: uuid
+  .extend({ keywords: z.string().optional(), mention: z.string().optional() })
   .omit({ mentions: true });
 
-export const createPost = form(createPostSchema, async (data) => {
+export const createPost = form(createPostSchema, async (data, issue) => {
   const { cookies } = getRequestEvent();
   const token = cookies.get("token");
+
+  const mention = data.mention ? await getMentionId(data.mention) : null;
+  if (data.mention && !mention) return invalid(issue.mention("User not found"));
 
   let response = await fetch(API_URL + "/posts", {
     method: "POST",
@@ -32,7 +44,7 @@ export const createPost = form(createPostSchema, async (data) => {
         .trim()
         .split(" ")
         .filter((keyword) => keyword.length),
-      mentions: data.mention ? [data.mention] : []
+      mentions: mention ? [mention] : []
     })
   });
 
@@ -41,7 +53,6 @@ export const createPost = form(createPostSchema, async (data) => {
       case 400:
         return invalid(...(await response.json()).errors);
       case 401:
-        console.log("Unauthorized, redirecting to signin");
         return redirect(303, "/signin");
       default:
         return error(500, "Something went wrong");
