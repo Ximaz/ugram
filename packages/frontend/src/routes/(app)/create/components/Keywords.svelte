e077d17394db4d4773cde9dcb0e2c397e43328e1
Clement-Z4RM <clement.lienard@epitech.eu>
2026-02-15 15:15:18 -0500

    feat(frontend): add error handling for post creation fields

--- packages/frontend/src/routes/(app)/create/components/Keywords.svelte
+++ packages/frontend/src/routes/(app)/create/components/Keywords.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { Label } from "$lib/components/ui/label";
   import { Textarea } from "$lib/components/ui/textarea";
+  import * as Field from "$lib/components/ui/field";
   import { createPost } from "$lib/remotes/post.remote";
   import Code from "../../components/Code.svelte";
 </script>
@@ -8,6 +9,9 @@
 <div class="grid w-full gap-1.5">
   <Label for="keywords">Keywords</Label>
   <Textarea {...createPost.fields.keywords.as("text")} id="keywords" />
+  {#each createPost.fields.keywords.issues() as issue, index (index)}
+    <Field.Error>{issue.message}</Field.Error>
+  {/each}
   <p class="text-xs text-muted-foreground">
     All non-alphanumeric characters will be considered as separators, except for the dash (<Code
       >-</Code
