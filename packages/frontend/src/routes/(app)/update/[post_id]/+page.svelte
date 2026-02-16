d460ef20159abd709ef09ef41f909f07c1121b31
Clement-Z4RM <clement.lienard@epitech.eu>
2026-02-15 23:43:42 -0500

    fix(frontend): expand container width for better layout

--- packages/frontend/src/routes/(app)/update/[post_id]/+page.svelte
+++ packages/frontend/src/routes/(app)/update/[post_id]/+page.svelte
@@ -18,7 +18,7 @@
   const post = $derived(await getPost(params.post_id));
 </script>
 
-<div class="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+<div class="flex min-h-svh w-full flex-col items-center justify-center gap-6 p-6 md:p-10">
   <div class="flex w-full max-w-sm flex-col gap-6">
     <Logo />
     <div class="flex flex-col gap-6">
