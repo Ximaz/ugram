ced1a9c7cd044ab4b496cae7351fc80bdac5b014
Clement-Z4RM <clement.lienard@epitech.eu>
2026-02-15 19:08:33 -0500

    feat(frontend): create `User` component for user profile display

--- packages/frontend/src/routes/(app)/user/[username]/+page.svelte
+++ packages/frontend/src/routes/(app)/user/[username]/+page.svelte
@@ -2,7 +2,7 @@
   import { resolve } from "$app/paths";
   import { Separator } from "$lib/components/ui/separator";
   import { getUser } from "$lib/remotes/user.remote";
-  import Avatar from "../../components/Avatar.svelte";
+  import User from "$lib/components/User.svelte";
 
   let { params } = $props();
 
@@ -10,13 +10,7 @@
 </script>
 
 <div class="mx-auto max-w-5xl p-7">
-  <div class="flex h-20 items-center gap-2 pl-2">
-    <Avatar src={user.profilePicture} username={user.username} />
-    <div>
-      <p class="text-xs">@{user.username}</p>
-      <p>{user.firstname} {user.lastname}</p>
-    </div>
-  </div>
+  <User {...user} />
   <Separator class="my-5" />
   <div class="grid grid-cols-3 gap-1 pt-2">
     {#each user.posts as post (post.id)}
