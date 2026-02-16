0feb103c74868db0b5c02e3d1268dad5462d0beb
zowks <67444066+zowks@users.noreply.github.com>
2026-02-16 00:23:34 +0000

    perf(frontend-navbar): Better navigation bar layout

--- packages/frontend/src/routes/(app)/+layout.svelte
+++ packages/frontend/src/routes/(app)/+layout.svelte
@@ -30,39 +30,37 @@
   ];
 </script>
 
-<div class="flex">
-  <nav class="sticky top-0 flex h-screen w-16 shrink-0 flex-col items-center justify-between p-2">
-    <Button variant="link" size="icon-lg" class="cursor-pointer">
-      <GalleryVerticalEndIcon />
-    </Button>
+<nav class="fixed top-0 flex h-screen w-16 shrink-0 flex-col items-center justify-between p-2">
+  <Button variant="link" size="icon-lg" class="cursor-pointer">
+    <GalleryVerticalEndIcon />
+  </Button>
 
-    <div class="flex flex-col gap-1">
-      {#each items as item (item.url)}
-        {@const Icon = item.icon}
-        <Tooltip.Root>
-          <Tooltip.Trigger>
-            <Button href={item.url} variant="ghost" size="icon-lg" class="cursor-pointer">
-              <Icon />
-            </Button>
-          </Tooltip.Trigger>
-          <Tooltip.Content side="right">
-            <p>{item.tooltip}</p>
-          </Tooltip.Content>
-        </Tooltip.Root>
-      {/each}
-    </div>
+  <div class="flex flex-col gap-1">
+    {#each items as item (item.url)}
+      {@const Icon = item.icon}
+      <Tooltip.Root>
+        <Tooltip.Trigger>
+          <Button href={item.url} variant="ghost" size="icon-lg" class="cursor-pointer">
+            <Icon />
+          </Button>
+        </Tooltip.Trigger>
+        <Tooltip.Content side="right">
+          <p>{item.tooltip}</p>
+        </Tooltip.Content>
+      </Tooltip.Root>
+    {/each}
+  </div>
 
-    <Button variant="link" size="icon-lg" href="/account">
-      <Avatar.Root>
-        <Avatar.Image />
-        <Avatar.Fallback>
-          <UserIcon />
-        </Avatar.Fallback>
-      </Avatar.Root>
-    </Button>
-  </nav>
+  <Button variant="link" size="icon-lg" href="/account">
+    <Avatar.Root>
+      <Avatar.Image />
+      <Avatar.Fallback>
+        <UserIcon />
+      </Avatar.Fallback>
+    </Avatar.Root>
+  </Button>
+</nav>
 
-  <main class="mr-16 grow">
-    {@render children?.()}
-  </main>
-</div>
+<main class="mx-16 flex flex-col items-center">
+  {@render children?.()}
+</main>
