ee52dee1ea4625f69a43c0644458d370978ba2d4
DURAND Malo <durand.malo.60590@gmail.com>
2026-03-09 10:06:34 +0100

    feat: JWT guard checks the token against logged out token in cache

--- packages/backend/src/auth/guards/jwt.guard.ts
+++ packages/backend/src/auth/guards/jwt.guard.ts
@@ -1,15 +1,23 @@
 import {
   CanActivate,
   ExecutionContext,
+  Inject,
   Injectable,
+  InternalServerErrorException,
   UnauthorizedException,
 } from '@nestjs/common';
 import { FastifyRequest } from 'fastify';
 import { AuthService } from '../auth.service.js';
+import { type Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { UserTokenDataDto } from '../entities/user-token-data.js';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    @Inject(CACHE_MANAGER) private readonly cacheService: Cache,
+  ) {}
 
   private static extractBearerToken(request: FastifyRequest): string {
     const authHeader = request.headers.authorization ?? '';
@@ -23,16 +31,27 @@ export class AuthGuard implements CanActivate {
     const request = context.switchToHttp().getRequest<FastifyRequest>();
     const token = AuthGuard.extractBearerToken(request);
 
+    let loggoutToken: string | undefined = undefined;
     try {
-      const payload = await this.authService.verifyToken(token);
+      loggoutToken = await this.cacheService.get(`loggout-${token}`);
+    } catch {
+      throw new InternalServerErrorException();
+    }
+    if (loggoutToken === token) {
+      throw new UnauthorizedException();
+    }
 
-      Object.defineProperty(request, 'user', {
-        configurable: true,
-        value: payload,
-      });
+    let payload: UserTokenDataDto;
+    try {
+      payload = await this.authService.verifyToken(token);
     } catch {
       throw new UnauthorizedException();
     }
+
+    Object.defineProperty(request, 'user', {
+      configurable: true,
+      value: payload,
+    });
     return true;
   }
 }
