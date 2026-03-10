2163fa9b1b36574a56cb4d04f8c8e57067ea98d3
zowks <67444066+zowks@users.noreply.github.com>
2026-03-10 21:29:19 +0000

    refactor(frontend): improved authentication pages code

--- packages/frontend/src/routes/(auth)/signin/+page.svelte
+++ packages/frontend/src/routes/(auth)/signin/+page.svelte
@@ -1,32 +1,46 @@
 <script lang="ts">
+  import { resolve } from "$app/paths";
+  import { signIn } from "$lib/remotes/auth.remote";
+
   import * as Card from "$lib/shadcn/card/index.js";
   import * as Field from "$lib/shadcn/field/index.js";
 
-  import Header from "$lib/components/Header.svelte";
-  import Logo from "$lib/components/Logo.svelte";
-  import Password from "./components/Password.svelte";
-  import Submit from "./components/Submit.svelte";
-  import Email from "./components/Email.svelte";
-
-  import { signIn } from "$lib/remotes/auth.remote";
+  import CardHeader from "$lib/components/CardHeader.svelte";
+  import FieldButton from "$lib/components/fields/FieldButton.svelte";
+  import LabeledInput from "$lib/components/fields/FieldInput.svelte";
 </script>
 
-<div class="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-  <div class="flex w-full max-w-sm flex-col gap-6">
-    <Logo />
-    <div class="flex flex-col gap-6">
-      <Card.Root>
-        <Header title="Welcome back" description="Login with your account" />
-        <Card.Content>
-          <form {...signIn}>
-            <Field.Group>
-              <Email />
-              <Password />
-              <Submit />
-            </Field.Group>
-          </form>
-        </Card.Content>
-      </Card.Root>
-    </div>
-  </div>
+<div class="flex flex-col gap-6">
+  <Card.Root>
+    <CardHeader title="Welcome back" description="Login with your account" />
+
+    <Card.Content>
+      <form {...signIn}>
+        <Field.Group>
+          <LabeledInput
+            required
+            label="email"
+            placeholder="jdoe@example.com"
+            {...signIn.fields.email.as("email")}
+            issues={signIn.fields.email.issues()}
+          />
+
+          <LabeledInput
+            required
+            label="password"
+            placeholder="Enter your password"
+            {...signIn.fields.password.as("password")}
+            issues={signIn.fields.password.issues()}
+          />
+
+          <FieldButton type="submit">
+            Login
+            {#snippet footer()}
+              Don't have an account? <a href={resolve("/signup")}>Sign up</a>
+            {/snippet}
+          </FieldButton>
+        </Field.Group>
+      </form>
+    </Card.Content>
+  </Card.Root>
 </div>
