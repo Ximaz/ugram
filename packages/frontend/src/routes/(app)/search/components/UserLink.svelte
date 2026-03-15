b36eeeb4045225f414a49e462a977e2620c9433d
zowks <67444066+zowks@users.noreply.github.com>
2026-03-15 23:04:59 +0000

    feat(frontend): implement `InfiniteScroll` component and integrate with new search

---
+++ packages/frontend/src/routes/(app)/search/components/UserLink.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { resolve } from "$app/paths";
+  import * as Avatar from "$lib/shadcn/avatar";
+  import { Button } from "$lib/shadcn/button";
+  import { UserIcon } from "@lucide/svelte";
+  import type { UserPartialData } from "backend/schemas";
+
+  let { username, profilePicture, firstname, lastname, id }: UserPartialData = $props();
+</script>
+
+<Button
+  variant="ghost"
+  class="h-16 cursor-pointer justify-start gap-3"
+  href={resolve(`/user/${id}`)}
+>
+  <Avatar.Root class="aspect-square h-full w-fit">
+    <Avatar.Image class="object-cover" src={profilePicture} alt={username} />
+    <Avatar.Fallback class="uppercase">
+      <UserIcon />
+    </Avatar.Fallback>
+  </Avatar.Root>
+
+  <div class="flex flex-col items-start">
+    <p class="text-xs">{firstname} {lastname}</p>
+    <p class="font-bold">{username}</p>
+  </div>
+</Button>
