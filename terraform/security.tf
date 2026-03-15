cd952e07af1b5fb506dd34ab620f0f2dbe68ceef
DURAND Malo <durand.malo.60590@gmail.com>
2026-03-15 19:33:08 -0400

    feat: adding the frontend deployment

--- terraform/security.tf
+++ terraform/security.tf
@@ -31,3 +31,47 @@ resource "aws_security_group" "redis" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "frontend" {
+  name        = "frontend-sg"
+  description = "Frontend service"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow HTTP from internet"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    security_groups = [aws_security_group.lb_frontend.id]
+  }
+
+  egress {
+    description     = "Allow backend access"
+    from_port       = 3000
+    to_port         = 3000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb.id]
+  }
+}
+
+resource "aws_security_group" "lb_frontend" {
+  name        = "frontend-alb-sg"
+  description = "Frontend ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "HTTP public"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
