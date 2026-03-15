f1d9989716d39bb88d65ec55b1151a1c44e796d6
zowks <67444066+zowks@users.noreply.github.com>
2026-03-15 22:55:53 +0000

    feat(schema): include missing user and post schemas in index export

--- packages/backend/src/index.schema.ts
+++ packages/backend/src/index.schema.ts
@@ -5,11 +5,13 @@ export * from './auth/schemas/user-token.schema.js';
 export * from './auth/schemas/user-token-data.schema.js';
 
 export * from './users/schemas/user-data.schema.js';
+export * from './users/schemas/user-data-list.schema.js';
 export * from './users/schemas/user-partial-data.schema.js';
 export * from './users/schemas/user-avatar-upload.schema.js';
 export * from './users/schemas/user-update-data.schema.js';
 
 export * from './posts/schemas/post-data.schema.js';
+export * from './posts/schemas/get-posts-list.schema.js';
 export * from './posts/schemas/post-data-list.schema.js';
 export * from './posts/schemas/created-post.schema.js';
 export * from './posts/schemas/post-create.schema.js';
