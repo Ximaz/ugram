0f6f1142a8ac7a0afd785f28414de47c2c173d79
DURAND Malo <durand.malo.60590@gmail.com>
2026-03-16 16:55:02 -0400

    fix: can't delete s3 files

--- packages/backend/src/users/users.service.ts
+++ packages/backend/src/users/users.service.ts
@@ -173,8 +173,9 @@ export class UsersService {
 
     if (user.profilePicture) {
       const url = new URL(user.profilePicture);
-      const key = url.pathname.substring('/static/avatars/'.length);
-      await this.s3Service.delete('avatars', key);
+      const filename = url.pathname.substring('/static/avatars/'.length);
+      const key = path.join('avatars', token.id, filename);
+      await this.s3Service.delete(this.staticService.getBucket(), key);
     }
 
     const posts = await this.prismaService.post.findMany({
